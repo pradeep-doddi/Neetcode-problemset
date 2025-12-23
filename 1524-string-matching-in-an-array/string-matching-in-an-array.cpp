9f6c6747bcecda6d9f48d9359393fbec4fea589f
pradeep <111519504+pradeep-doddi@users.noreply.github.com>
2025-12-23 10:22:31 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (50.87%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -4,19 +4,16 @@ public:
         vector<string>ans;
         for(int i=0;i<words.size();i++){
             string first=words[i];
-            bool flag= false;
             for(int j=0;j<words.size();j++){
                 if(i != j && words[j].find(first) != string::npos ){
-                    if(flag==false){
                         ans.push_back(first);
-                        flag=true;
+                        break;
                     }
                     
 
                 }
             }
            
-        }
          return ans;   
     }
 };
