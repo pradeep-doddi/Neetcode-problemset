e0a6ea3616fd4892c279d56ad01169d23342bcea
pradeep <111519504+pradeep-doddi@users.noreply.github.com>
2026-01-06 13:46:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (65.90%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(auto i : s ){
+            if(i=='(' || i=='{' || i=='['){
+                st.push(i);
+            }
+            else{
+                if(st.size() == 0) return false;
+                if(i==')' && st.top()=='(' || i=='}' && st.top()=='{' || i==']' && st.top()=='['){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.size()==0?true:false;
+    }
+};
