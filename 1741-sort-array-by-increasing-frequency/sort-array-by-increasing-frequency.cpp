8b611fe6f44ec6586b733b4fbbec43f81d491539
pradeep <111519504+pradeep-doddi@users.noreply.github.com>
2026-07-17 17:03:29 +0530

    Time: 4 ms (33.08%) | Memory: 16.3 MB (5.00%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-    static bool cmp(pair<int,int>mp1,pair<int,int>mp2){
-        if(mp1.second==mp2.second){
-            return mp1.first>mp2.first;
-        }
-        return mp1.second<mp2.second;
-    }
     vector<int> frequencySort(vector<int>& nums) {
         map<int,int>mp;
         for(auto i : nums){
             mp[i]++;
         }
-        vector<pair<int,int>>vec(mp.begin(),mp.end());
-        sort(vec.begin(),vec.end(),cmp);
-        vector<int>ans;
-        for(auto p : vec){
-            while(p.second--){
-                ans.push_back(p.first);
-            }
-        }
-        return ans;
+        sort(nums.begin(),nums.end(),[&](int a,int b){
+            if(mp[a]==mp[b]) return a>b;
+            return mp[a] < mp[b];
+        });
+        return nums;
+       
     }
 };
