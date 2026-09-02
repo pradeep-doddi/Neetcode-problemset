3f9cae044cda27ec9e1d2e4adb99d5ea5808bcc9
pradeep <111519504+pradeep-doddi@users.noreply.github.com>
2026-09-02 18:23:00 +0530

    Time: 10 ms (5.35%) | Memory: 79.5 MB (20.40%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -10,39 +10,37 @@
  */
 class Solution {
 public:
-    ListNode* reversel(ListNode* head){
-        ListNode*prev=nullptr;
-        ListNode*curr=head;
-        while(curr){
-            ListNode*next=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=next;
-        }
-        return prev;
-    }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        l1=reversel(l1);
-        l2=reversel(l2);
-        ListNode*dummy=new ListNode(0);
-        ListNode*tail=dummy;
-        int sum=0,carry=0;
-        while(l1 || l2 || carry){
-            sum=carry;
-            if(l1){
-                sum += l1->val;
-                l1=l1->next;
-            }
-            if(l2){
-                sum+=l2->val;
-                l2=l2->next;
-            }
-            int num=sum%10;
-            carry=sum/10;
-            ListNode* temp = new ListNode(num);
-            tail->next=temp;
-            tail=temp;
+        stack<int>s1;
+        stack<int>s2;
+        ListNode* dummy=nullptr;
+        while(l1){
+            s1.push(l1->val);
+            l1=l1->next;
         }
-        return reversel(dummy->next);
+        while(l2){
+            s2.push(l2->val);
+            l2=l2->next;
+        }
+        int carry=0;
+        while(!s1.empty()|| !s2.empty() || carry){
+            int sum=carry;
+            if(!s1.empty()){
+                int val=s1.top();
+                sum += val;
+                s1.pop();
+            }
+            if(!s2.empty()){
+                int val=s2.top();
+                sum += val;
+                s2.pop();
+            }
+            int newans=sum%10;
+            carry=sum/10;
+            ListNode* temp = new ListNode(newans);
+            temp->next=dummy;
+            dummy=temp;
+        }
+        return dummy;
     }
 };
